8265745caa838cec71a5d27805f908f889e5962f
源源源 <2632395964@qq.com>
2020-06-11 11:59:20 +0800

    将svg代码整理到icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
   <button class= 'g-button' :class="{[`icon-${iconPosition}`]: true}">
-    <svg v-if="icon" class="icon">
-      <use :xlink:href="`#i-${icon}`"></use>
-  </svg>
+  <g-icon v-if="icon" :name="icon"></g-icon>
   <div class="content">
     <slot></slot>
   </div>
@@ -12,7 +10,15 @@
 
 <script>
 export default {
-  props:['icon', 'iconPosition']
+  // props:['icon', 'iconPosition']
+  props:{
+    icon:{},
+    iconPosition:{
+      type:String,
+      default:'left'
+    }
+
+  }
 }
 </script>
 <style lang="scss">
