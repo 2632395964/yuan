80333a95366a44097763813053e7d9d6e8389dc3
源源源 <2632395964@qq.com>
2020-06-12 18:50:46 +0800

    添加input样式

--- src/input.vue
+++ src/input.vue
@@ -3,8 +3,8 @@
     <input v-bind:value="value" type="text"  :disabled="disabled" 
     :readonly="readonly" >
     <template v-if="error">
-      <icon name="error"></icon>
-      <span>{{error}}</span>
+      <icon name="error" class="icon-error"></icon>
+      <span class="errorMessage">{{error}}</span>
     </template>
   </div>
 </template>
@@ -41,9 +41,14 @@ $border-color-hover: #666;
 $border-radious: 4px;
 $font-size: 12px;
 $box-shadow-color: rgba(0,0,0,0.5);
+$red: #f1453d;
 .wrapper{
   font-size: $font-size;
-  display: inline-block;
+  display: inline-flex;
+  align-items: center;
+  > :not(:last-child){
+    margin-right: .5em;
+  }
   > input{
     height: 32px;
     border: 1px solid $border-color;
@@ -62,6 +67,17 @@ $box-shadow-color: rgba(0,0,0,0.5);
       color: #bbb;
       cursor: not-allowed;
     }
+  }
+  &.error{
+    > input{
+      border-color: $red;
+    }
+  }
+  .icon-error{
+    fill: $red;
+  }
+  .errorMessage{
+    color: $red;
   }
 }
 
