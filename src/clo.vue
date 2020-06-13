9f4e98bacc8ea7c3ca4755899472076a395d6738
源源源 <2632395964@qq.com>
2020-06-13 09:20:14 +0800

    row和clo的雏形完成

---
+++ src/clo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="clo" :class="[`clo-${span}`]" >
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    span:{
+      type:[Number,String]
+    } 
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .clo{
+    height: 100px;
+    background: grey;
+    width: 50%;
+    border: 1px solid red;
+
+    $class-prefix: clo-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24)*100%;
+      }
+    }
+  }
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="clo">
+    <slot></slot>
+  </div>
+</template>
+<style lang="scss" scoped>
+.clo{
+  display: flex;
+}
+
+</style>
