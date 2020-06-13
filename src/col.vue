fe08772f64509b33a363e1e909d4f0dde002db0c
源源源 <2632395964@qq.com>
2020-06-13 17:42:09 +0800

    完成响应式布局

--- src/col.vue
+++ src/col.vue
@@ -5,6 +5,16 @@
   </div>
 </template>
 <script>
+let validator = (value) => {
+  let keys = Object.keys(value)
+  let valid = true
+  keys.forEach(key => {
+    if(!['span','offset'].includes(key)){
+      valid=false
+    }
+  })
+  return valid
+}
 export default {
   props:{
     span:{
@@ -12,6 +22,22 @@ export default {
     },
     offset:{
       type:[Number,String]
+    },
+    phone: {
+      type: Object,
+      validator,
+    },
+    ipad: {
+      type: Object,
+      validator,
+    },
+    narrowPc:{
+      type: Object,
+      validator,
+    },
+    widePc:{
+      type: Object,
+      validator,
     }
   },
   data () {
@@ -27,10 +53,15 @@ export default {
       }
     },
     colClass () {
-      let {span,offset} = this
+      let {span,offset,phone,ipad,narrowPc,widePc} = this
+      let phoneClass = []
       return [
         span && `col-${span}`,
-        offset && `offset-${offset}`
+        offset && `offset-${offset}`,
+        ...(phone ? [`col-phone-${phone.span}`] : []),
+        ...(ipad ? [`col-ipad-${ipad.span}`] : []),
+        ...(narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
+        ...(widePc ? [`col-wide-pc-${widePc.span}`] : [])
       ]
     }
   }
@@ -40,7 +71,7 @@ export default {
   .col{
     height: 100px;
     background: grey;
-    width: 50%;
+    // width: 50%;
     border: 1px solid red;
 
     $class-prefix: col-;
@@ -53,6 +84,62 @@ export default {
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n/24)*100%;
+      }
+    }
+    @media (max-width: 576px) {
+      $class-prefix: col-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n/24)*100%;
+        }
+      }
+      $class-prefix: offset-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n/24)*100%;
+        }
+      }
+    }
+    @media (min-width: 577px) and (max-width: 768px) {
+      $class-prefix: col-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n/24)*100%;
+        }
+      }
+      $class-prefix: offset-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n/24)*100%;
+        }
+      }
+    }
+    @media (min-width: 769px) and (max-width: 992px) {
+      $class-prefix: col-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n/24)*100%;
+        }
+      }
+      $class-prefix: offset-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n/24)*100%;
+        }
+      }
+    }
+    @media (min-width: 1201px) {
+      $class-prefix: col-wide-pc;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n/24)*100%;
+        }
+      }
+      $class-prefix: offset-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n/24)*100%;
+        }
       }
     }
   }
