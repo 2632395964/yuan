1132d3bc7d2e3002212407576e205ef17631853d
源源源 <2632395964@qq.com>
2020-06-15 10:03:21 +0800

    完成toast组件

--- src/app.js
+++ src/app.js
@@ -5,30 +5,28 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Sider from './sider'
 import Content from './content'
 import Footer from './footer'
-import Layout from './layout'
-import Sider from './sider'
-import Header from './header'
 import Toast from './toast'
 import plugin from './plugin'
-
-
-
-
-Vue.component('g-button',Button)
+Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
-Vue.component('g-button-group',ButtonGroup)
-Vue.component('g-input',Input)
-Vue.component('g-row',Row)
-Vue.component('g-col',Col)
-Vue.component('g-header',Header)
-Vue.component('g-footer',Footer)
-Vue.component('g-content',Content)
-Vue.component('g-layout',Layout)
-Vue.component('g-sider',Sider)
-Vue.component('g-toast',Toast)
+Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
+Vue.component('g-toast', Toast)
 Vue.use(plugin)
+import createElement from 'vue'
+const h = createElement
 
 
 
@@ -41,11 +39,33 @@ new Vue({
     loading3: false,
     message: 'hi'
   },
-  created () {
+  created(){
   },
   methods: {
-    showToast(){
-      this.$toast('我是message')
+    // showToast(){
+    showToast1(){
+      this.showToast('top')
+    },
+    showToast2(){
+      this.showToast('middle')
+    },
+    showToast3(){
+      this.showToast('bottom')
+    },
+    showToast(position){
+      this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
+        // position: 'bottom',
+        position,
+        enableHtml: false,
+        closeButton: {
+          text: '已充值',
+          callback () {
+            console.log('他说已经充值智商了')
+          }
+        },
+        autoClose: false,
+        autoCloseDelay: 3
+      })
     }
   }
 })
--- src/plugin.js
+++ src/plugin.js
@@ -1,21 +1,33 @@
 import Toast from './toast'
+let currentToast
 export default {
-  install(Vue, options){
-    Vue.prototype.$toast = function(message){
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: {
-          closeButton:{
-            text: '知道了',
-            callback() {
-            console.log('用户说他知道了')
-            }
-          }
+  install (Vue, options) {
+    Vue.prototype.$toast = function (message, toastOptions) {
+      if (currentToast) {
+        currentToast.close()
+      }
+      // currentToast = createToast({Vue, message, propsData: toastOptions})
+      currentToast = createToast({
+        Vue,
+        message,
+        propsData: toastOptions,
+        onClose: () => {
+          currentToast = null
         }
       })
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
     }
   }
+}
+
+
+/* helpers */
+// function createToast ({Vue, message, propsData}) {
+function createToast ({Vue, message, propsData, onClose}) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({propsData})
+  toast.$slots.default = [message]
+  toast.$mount()
+  toast.$on('close', onClose)
+  document.body.appendChild(toast.$el)
+  return toast
 }
